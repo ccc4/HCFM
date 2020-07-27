c1ea043f76c643d4e17539a5fde85d81de3b3d90
csh <csh@wincubemkt.com>
2020-07-27 17:19:41 +0900

    upload

---
+++ src/main/java/com/HCFM/HcfmApplication.java
@@ -0,0 +1,13 @@
+package com.HCFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcfmApplication.class, args);
+	}
+
+}
